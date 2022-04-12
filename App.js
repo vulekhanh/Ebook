d54089232181c3aeb1f561e29f4b6c33883032f0
vulekhanh <vulekhanh69@gmail.com>
2022-04-12 22:52:09 +0700

    initialized repo

---
+++ App.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import SignIn from './Screens/SignIn';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  return <SignIn />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ Screens/SignIn.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+const SignIn = () => {
+  return (
+    <View>
+      <Icon name="twitter" size={50} color="#179cf0" />
+    </View>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({});
