b5f8204ec2458376bf896c55dc9b8d0b1efb6199
vulekhanh <vulekhanh69@gmail.com>
2022-04-17 11:15:14 +0700

    fixed scroll view for signUpView()

--- App.js
+++ App.js
@@ -1,45 +1,49 @@
 import React from 'react';
-import { createStackNavigator } from "@react-navigation/stack";
-import  {NavigationContainer, DefaultTheme } from '@react-navigation/native';
-
-import BookDetail from "./Screens/BookDetail";
-import Tabs from "./navigation/tabs";
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import BookDetail from './Screens/BookDetail';
+import Tabs from './navigation/tabs';
+import SigningScreen from './Screens/SingingScreen';
 
 const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        border: "transparent"
-    }
-}
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: 'transparent',
+  },
+};
 
 const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-      <NavigationContainer theme={theme}>
+  return (
+    <NavigationContainer theme={theme}>
       <Stack.Navigator
-          screenOptions={{
-              headerShown: false
-          }}        
-          initialRouteName={'Home'}
-      >
-          {/* Tabs */}
-          <Stack.Screen 
-          name="Home" 
-          component={Tabs} 
-          options={{ headerShown: false }}
-          />
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={'SigningScreen'}>
+        {/* Tabs */}
+        <Stack.Screen
+          name="SigningScreen"
+          component={SigningScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Tabs}
+          options={{headerShown: false}}
+        />
 
-          {/* Screens */}
-          <Stack.Screen 
-          name="BookDetail" 
-          component={BookDetail} 
-          options={{ headerShown: false }} 
-          />
+        {/* Screens */}
+        <Stack.Screen
+          name="BookDetail"
+          component={BookDetail}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
-  </NavigationContainer>
-    )
-}
+    </NavigationContainer>
+  );
+};
 
 export default App;
